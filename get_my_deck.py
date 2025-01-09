3cb7776e14ad6b366156fc793afa21d64e4b6141
MaximumFish <5391232+MaximumFish@users.noreply.github.com>
2025-01-09 14:29:11 +0000

    Add files via upload
    Changed Twilio to Telegram, added crash alerting, added random delay between checks, fixed index out of range issue.

--- get_my_deck.py
+++ get_my_deck.py
@@ -1,19 +1,23 @@
-from selenium.webdriver.common.by import By
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import time
-from twilio.rest import Client
+from random import randint
 from webdriver_manager.chrome import ChromeDriverManager
 from datetime import datetime
+import requests
 
-account_sid = "from_twilio" ## SIGN UP FOR A FREE TWILIO ACCOUNT AND GET YOUR SID AND AUTH TOKEN 
-auth_token  = "from_twilio" ## AS ABOVE
-client = Client(account_sid, auth_token)
 
 browser_options = Options()
 browser_options.add_argument("--headless=new")
 url = "https://store.steampowered.com/sale/steamdeckrefurbished"
+
+bot_token = "00000000:00000000000000000000" # Your Telegram bot token here
+chat_id = 00000000 # Your chat ID to send alerts to
+    
 
 def start():
     service = Service(ChromeDriverManager().install())
@@ -33,19 +37,23 @@ def quit(driver):
 
 
 def runner(driver):
-    all_btn = driver.find_elements(By.XPATH, "//*[@id='SaleSection_33131']")
-    x = all_btn[0].text
-    x = x.split("00")[0]  ## HERE YOU CAN BREAKPOINT AND DETERMINE WHICH PART OF THE TEXT YOU ARE INTERESTED IN (E.G. STRIP FOR 64GB, 256GB ETC
-    print(x)
-    if "add" in x.lower():
-        message = client.messages.create(
-            to="+44777777777", ## THE NUMBER THE ALERT WILL GO TO
-            from_="+44777777777", ## THE NUMBER PROVIDED BY TWILIO
-            body="In Stock https://store.steampowered.com/sale/steamdeckrefurbished")
-        status = 1
-    else:
+    #all_btn = driver.find_elements(By.XPATH, "//*[@id='SaleSection_33131']")
+    try:
+        all_btn = WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located((By.XPATH, "//*[@id='SaleSection_33131']")))
+        x = all_btn[0].text
+        x = x.split("00")[0]  ## HERE YOU CAN BREAKPOINT AND DETERMINE WHICH PART OF THE TEXT YOU ARE INTERESTED IN (E.G. STRIP FOR 64GB, 256GB ETC
         print(x)
-        print(datetime.now())
+        if "add" in x.lower():
+            message = "OMG Steamdeck available! https://store.steampowered.com/sale/steamdeckrefurbished/"
+            notify_url = f"https://api.telegram.org/bot{bot_token}/sendMessage?chat_id={chat_id}&text={message}"
+            requests.get(notify_url) # this sends the message
+            status = 1
+        else:
+            print(x)
+            print(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+            status = 0
+    except Exception as e:
+        print(f"Error: {e}")
         status = 0
     return status
 
@@ -61,7 +69,9 @@ def get_my_deck():
                 status = runner(driver)
                 if status == 1:
                     break
-                time.sleep(20) ## HOW OFTEN TO CHECK THE WEBSITE
+                randDelay = randint(10,60) # Random delay from 10 to 60 seconds.
+                print(f'Next check in {randDelay} seconds.')
+                time.sleep(randDelay)
                 c = c+1
                 refresh(driver)
             else:
@@ -76,5 +86,9 @@ def get_my_deck():
             time.sleep(20) ## DO NOT EDIT
             get_my_deck()
 
-
-get_my_deck()
+try:
+    get_my_deck()
+except:
+    message = "Get-my-deck crashed. :("
+    notify_url = f"https://api.telegram.org/bot{bot_token}/sendMessage?chat_id={chat_id}&text={message}"
+    requests.get(notify_url)
